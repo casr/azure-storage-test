940988502ca49e8be782e80a5d05f93899e873ed
Chris Rawnsley <chris@puny.agency>
2017-07-20 21:56:22 +0100

    Upload everything from a `to-upload` directory

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv/config')
+const fs = require('fs')
+const path = require('path')
 
 const azure = require('azure-storage')
 const blobService = azure.createBlobServiceWithSas(
@@ -6,18 +8,27 @@ const blobService = azure.createBlobServiceWithSas(
   process.env.AZURE_STORAGE_SHARED_ACCESS_SIGNATURE
 )
 
-blobService.createBlockBlobFromLocalFile(
-  process.env.AZURE_STORAGE_CONTAINER_NAME,
-  'casr-delete-me.jpg',
-  'picture.jpg',
-  function (err, result, response) {
-    if (err) {
-      console.error('error')
-      console.error(err)
-      return
-    }
+const directory = path.join(__dirname, './to-upload')
 
-    console.log('result', result)
-    console.log('response', response)
+fs.readdir(directory, (err, files) => {
+  if (err) {
+    console.log(err)
+    return
   }
-)
+
+  files.forEach(file => {
+    blobService.createBlockBlobFromLocalFile(
+      process.env.AZURE_STORAGE_CONTAINER_NAME,
+      process.env.UPLOAD_BASE + file,
+      path.join(directory, file),
+      function (err, result, response) {
+        if (err) {
+          console.error('error', err)
+          return
+        }
+
+        console.log('Uploaded ' + file)
+      }
+    )
+  })
+})
